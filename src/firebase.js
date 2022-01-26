0b8abd06c8ca07c2dbd3a788f82e3a965c5ce136
Gerardo Rodriguez <gdrodriguezok@gmail.com>
2022-01-26 15:46:00 -0300

    env + readme + index title

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBVTfm7P8zHljaUhw3btiZSCpldyIpaIXo",
-  authDomain: "elolimpio-d7fb6.firebaseapp.com",
-  projectId: "elolimpio-d7fb6",
-  storageBucket: "elolimpio-d7fb6.appspot.com",
-  messagingSenderId: "618508325421",
-  appId: "1:618508325421:web:6d4005cc097272b7549ea4",
-  measurementId: "G-P8GECP5YWB",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId,
 };
 
 const app = initializeApp(firebaseConfig);
